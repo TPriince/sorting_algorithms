4cb8f6cdc9a174af87c430e42b1f84452531e6e6
TPriince <tobiloba110@gmail.com>
2022-07-20 09:26:39 -0700

    Task 0 - Bubble sort

--- sort.h
+++ sort.h
@@ -1,6 +1,18 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef SWAP
+#define SWAP(array, a, b) \
+	{                       \
+		tmp = array[a];       \
+		array[a] = array[b];  \
+		array[b] = tmp;       \
+	}
+#endif
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,6 +31,7 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 
 #endif /* SORT_H */
