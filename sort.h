fca314aaebabb672546a451ff883ec871fb4db7c
TPriince <tobiloba110@gmail.com>
2022-07-20 09:52:49 -0700

    Task 1 - Insertion sort

--- sort.h
+++ sort.h
@@ -32,6 +32,9 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *l, listint_t *r);
+void insertion_sort_list2(listint_t **list);
 
 
 #endif /* SORT_H */
